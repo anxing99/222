c3ce437b8f35bee0aa16770262fcfe04d59b6919
anxing99 <512652580@qq,com>
2019-03-07 09:11:59 +0800

    3.1.2
    常规修改

---
+++ 蓝信Mac/files/功能结构图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,H,I,H,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[_(X,Y,Z,j,ba,bb,q,bc,bd,bc,be,bf,v,_(bg,_(bh,bi,bj,bk),w,bl),T,_(),bm,_(),bn,_(bo,bp))])),bq,_(),br,_(bs,_(bt,bu)));}; 
+var b="url",c="功能结构图.html",d="generationDate",e=new Date(1551867533726.94),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="176feff35fa749a3a74bd2d30ac244a7",q="type",r="Axure:Page",s="name",t="功能结构图",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="near",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="id",Y="bce1bcfd4f664920aa43eb5be4443428",Z="label",ba="friendlyType",bb="图片",bc="imageBox",bd="styleType",be="visible",bf=true,bg="size",bh="width",bi=1033,bj="height",bk=4667,bl="75a91ee5b9d042cfa01b8d565fe289c0",bm="imageOverrides",bn="images",bo="normal~",bp="images/功能结构图/u126.jpg",bq="masters",br="objectPaths",bs="bce1bcfd4f664920aa43eb5be4443428",bt="scriptId",bu="u126";
+return _creator();
+})());
